b4300e68ea9d0874043ec748705835724429ad59
Zhenbang You <zhenbang_you@apple.com>
2023-06-28 19:33:30 -0700

    unsafe vec add

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,13 @@ fn main() {
     let b = vec![2; vec_len];
     let mut c = vec![0; vec_len];
     add_vec(&a, &b, &mut c, num_threads);
+    add_vec_unsafe(
+        I32ConstPtrWrapper(&a[0] as *const i32),
+        I32ConstPtrWrapper(&b[0] as *const i32),
+        I32MutPtrWrapper(&mut c[0] as *mut i32),
+        vec_len,
+        num_threads,
+    );
     let _: Vec<_> = c.iter().map(|x: &i32| println!("{x}")).collect();
 
     println!();
@@ -73,6 +80,49 @@ fn add_vec_slice(a: Box<&[i32]>, b: Box<&[i32]>, c: Box<&mut [i32]>) {
     for i in 0..a.len() {
         c[i] = a[i] + b[i];
     }
+}
+
+#[derive(Clone, Copy)]
+struct I32ConstPtrWrapper(*const i32);
+
+unsafe impl Send for I32ConstPtrWrapper {}
+unsafe impl Sync for I32ConstPtrWrapper {}
+
+#[derive(Clone, Copy)]
+struct I32MutPtrWrapper(*mut i32);
+
+unsafe impl Send for I32MutPtrWrapper {}
+unsafe impl Sync for I32MutPtrWrapper {}
+
+fn add_vec_unsafe(
+    a: I32ConstPtrWrapper,
+    b: I32ConstPtrWrapper,
+    c: I32MutPtrWrapper,
+    n: usize,
+    num_slices: usize,
+) {
+    let num_slices = std::cmp::min(n, num_slices);
+    let slice_len = (n + num_slices - 1) / num_slices;
+
+    thread::scope(|s| {
+        let _: Vec<_> = (0..num_slices)
+            .into_iter()
+            .map(|i| {
+                s.spawn(move || {
+                    for j in (slice_len * i)..(std::cmp::min(slice_len * (i + 1), n)) {
+                        let k = j as isize;
+                        let a = a;
+                        let b = b;
+                        let c = c;
+                        unsafe {
+                            *(c.0).offset(k) = *(a.0).offset(k) + *(b.0).offset(k);
+                        }
+                    }
+                })
+            })
+            .map(|h| h.join())
+            .collect();
+    })
 }
 
 fn split_immut_matrix<T>(
